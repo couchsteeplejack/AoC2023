8b6488bcaae9dc336237955ac602f6dda11385d0
mac <no@reply.com>
2023-12-18 20:29:02 +0100

    recursive

--- day17/day17.py
+++ day17/day17.py
@@ -1,7 +1,7 @@
 map = []
 with open("input.example1.txt") as f:
     for line in f:
-        map.append( [ i for i in line.strip() ])
+        map.append( [ int(i) for i in line.strip() ])
 
 xmax = len(map[0])
 ymax = len(map)
@@ -13,12 +13,14 @@ def walk(x, y, heatloss, hist):
     
     # which next steps (exclude hist)?
     neighbors = [(x+i, y+j) for i, j in [(1,0), (-1,0), (0,1), (0,-1)]]
-    # ...
+    for i in neighbors:
+        if i in hist:
+            neighbors.remove(i)
     
     # recursive call
+    results = [walk(i[0], i[1], heatloss+map[i[1]][i[0]], hist[:]+[(i[0],i[1])]) for i in neighbors]
 
     # return minimum loss of recurive calls
+    return min(results)
 
-    pass
-
-print(map)
+print(walk(0,0,0,[]))
